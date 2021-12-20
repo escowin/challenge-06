04f1589b6fb89795082ab9ef057b5d27b5c61800
escowin <edwin@escowinart.com>
2021-12-19 19:45:11 -0600

    js psuedocode & base vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,51 @@
-// AS A traveler
-// I WANT to see the weather outlook for multiple cities
-// SO THAT I can plan a trip accordingly
+// #1 see the weather outlook for multiple cities
 
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+// #2 a weather dashboard with form inputs
+var cityInput = function() {
+    // type city in search bar, hit search submit button, city data is retrieved
+};
+
+// #3 search for a city, presented with current and future conditions for that city and that city is added to the search history
+var saveCity = function() {
+    // current conditions
+    // future conditions
+    // save city to search history (localStorage)
+};
+
+// #4 view current weather conditions for that city, presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+var cityWeather = function() {
+    // city name
+    // Date
+    // weather condition icon
+    // temperature
+    // humidity
+    // wind speed
+    // UV index
+        // #5 view the UV index, presented with a color that indicates whether the conditions are:
+        // if favorable
+        // else if moderate
+        // else if severe
+
+};
+
+// #6 for that city, presented with a 5-day forecast
+var fiveDayForecast = function() {
+    // date
+    // weather condition icon
+    // temperature
+    // wind speed
+    // humidity
+};
+
+// #7 click on a city in the search history, presented with current and future conditions for that city
+var cityHistory = function() {
+    // save searched cities into localStorage
+    // previous searched cities are displayed in the aside.
+    // these cities are clickable 
+}
+
+
+// event listener clicks
+
+
+https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
