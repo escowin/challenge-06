b64cfee3d5b3ddf06307b4f17389e0d2fb1fc1d6
escowin <edwin@escowinart.com>
2021-12-19 21:54:56 -0600

    API set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,57 @@
-// #1 see the weather outlook for multiple cities
+var APIKey = "0962492a18dc2e6ff1c567a057f043f2";
+var city = document.querySelector('.city'); 
+var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-// #2 a weather dashboard with form inputs
-var cityInput = function() {
-    // type city in search bar, hit search submit button, city data is retrieved
-};
+// var button = document.querySelector('.button')
+// var inputValue = document.querySelector('.inputValue')
+// var city = document.querySelector('.city')
+// var temp = document.querySelector('.temp')
+// var wind = document.querySelector('.wind')
+// var uvIndex = document.querySelector('.uvIndex')
 
-// #3 search for a city, presented with current and future conditions for that city and that city is added to the search history
-var saveCity = function() {
-    // current conditions
-    // future conditions
-    // save city to search history (localStorage)
-};
+fetch(queryURL) 
 
-// #4 view current weather conditions for that city, presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-var cityWeather = function() {
-    // city name
-    // Date
-    // weather condition icon
-    // temperature
-    // humidity
-    // wind speed
-    // UV index
-        // #5 view the UV index, presented with a color that indicates whether the conditions are:
-        // if favorable
-        // else if moderate
-        // else if severe
+// // #1 see the weather outlook for multiple cities
 
-};
+// // #2 a weather dashboard with form inputs
+// var cityInput = function() {
+//     // type city in search bar, hit search submit button, city data is retrieved
+// };
 
-// #6 for that city, presented with a 5-day forecast
-var fiveDayForecast = function() {
-    // date
-    // weather condition icon
-    // temperature
-    // wind speed
-    // humidity
-};
+// // #3 search for a city, presented with current and future conditions for that city and that city is added to the search history
+// var saveCity = function() {
+//     // current conditions
+//     // future conditions
+//     // save city to search history (localStorage)
+// };
 
-// #7 click on a city in the search history, presented with current and future conditions for that city
-var cityHistory = function() {
-    // save searched cities into localStorage
-    // previous searched cities are displayed in the aside.
-    // these cities are clickable 
-}
+// // #4 view current weather conditions for that city
+// var cityWeather = function() {
+//     // city name & date
+//     // weather condition icon
+//     // temperature
+//     // humidity
+//     // wind speed
+//     // UV index
+//         // #5 view the UV index, presented with a color that indicates whether the conditions are:
+//         // if favorable
+//         // else if moderate
+//         // else if severe
 
+// };
 
-// event listener clicks
+// // #6 for that city, presented with a 5-day forecast
+// var fiveDayForecast = function() {
+//     // date
+//     // weather condition icon
+//     // temperature
+//     // wind speed
+//     // humidity
+// };
 
-
-https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// // #7 click on a city in the search history, presented with current and future conditions for that city
+// var cityHistory = function() {
+//     // save searched cities into localStorage
+//     // previous searched cities are displayed in the aside.
+//     // these cities are clickable 
+// };
