ec71ab8d87a5b947da955118becfc1829715729c
escowin <edwin@escowinart.com>
2022-04-21 06:48:23 -0500

    js:12-33 psuedocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,27 @@ var searchHistory = [];
 var searchBtnEl = document.querySelector("#search-btn");
 var searchInputEl = document.querySelector("#search-input");
 var searchHistoryEl = document.querySelector("#search-history");
-var todayEl = document.querySelector("#today");
+var currentWeatherEl = document.querySelector("#current-weather");
 var forecastEl = document.querySelector("#forecast");
+
+// SEARCH FOR CITY
+function searchCity() {};
+
+// DISPLAY CURRENT CITY'S WEATEHER & FORECAST
+function displayCurrentWeather() {
+  /*
+  <article class="card">
+    <h3 class="city">City</>
+    // date
+    // weather conditions icon
+    <div>
+      <p>temp
+      <p>humidity
+      <p>wind speed
+      <p>UV index
+    </div>
+  */
+};
+
+
+function displayForecast() {};
