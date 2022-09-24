1bcb24907cd1b5c5feb3c156d8f92e696f64e14d
escowin <edwin@escowinart.com>
2022-09-23 23:17:15 -0500

    can display relevant weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ var searchFormEl = document.querySelector("#search-form")
 var searchInputEl = document.querySelector("#search-input");
 var searchBtnEl = document.querySelector("#search-btn");
 var searchHistoryEl = document.querySelector("#search-history");
-var currentCityEl = document.querySelector("#current-city");
 var currentWeatherEl = document.querySelector("#current-weather");
 var forecastEl = document.querySelector("#forecast");
 
@@ -13,7 +12,6 @@ var getWeather = function(city) {
   var location = city;
   var key = "0962492a18dc2e6ff1c567a057f043f2";
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=imperial`;
-  console.log(url);
 
   fetch(url).then(function(response){
     response.json().then(function(data) {
@@ -23,10 +21,24 @@ var getWeather = function(city) {
 };
 
 // logic.display weather data
-var displayCurrentWeather = function(data) {
-  console.log(data.name);
+var displayCurrentWeather = function(city) {
+    console.log(city);
 
-}
+  // dom elements
+  var currentCityEl = document.getElementById("current-city");
+  var tempEl = document.getElementById("temp");
+  var humidityEl = document.getElementById("humidity");
+  var windSpeedEl = document.getElementById("windspeed");
+  var currentConditionsEl = document.getElementById("current-conditions");
+
+  // display data onto page
+  currentCityEl.textContent = city.name;
+  tempEl.textContent = `${city.main.temp}\u00B0`;
+  humidityEl.textContent = `${city.main.humidity}%`;
+  windSpeedEl.textContent = `${city.wind.speed} mph`;
+  currentConditionsEl.textContent = city.weather[0].description;
+};
+
 // logic.display current year
 var copyrightYear = function() {
   var year = new Date().getFullYear();
@@ -36,4 +48,4 @@ var copyrightYear = function() {
 };
 
 copyrightYear();
-getWeather("London");
+getWeather("Austin");
