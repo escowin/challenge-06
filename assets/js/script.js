511003551abb21ba811f3cf35e731bda1a8985f3
escowin <edwin@escowinart.com>
2022-09-23 22:38:40 -0500

    fetching api with location; passing api data into displayCurrentWeather

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,33 @@ var currentCityEl = document.querySelector("#current-city");
 var currentWeatherEl = document.querySelector("#current-weather");
 var forecastEl = document.querySelector("#forecast");
 
-// data.api set up
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
-var apiKey = "&appid=0962492a18dc2e6ff1c567a057f043f2&units=imperial";
 
+// logic.fetching api data
+var getWeather = function(city) {
+  var location = city;
+  var key = "0962492a18dc2e6ff1c567a057f043f2";
+  var url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=imperial`;
+  console.log(url);
+
+  fetch(url).then(function(response){
+    response.json().then(function(data) {
+      displayCurrentWeather(data);
+    });
+  });
+};
+
+// logic.display weather data
+var displayCurrentWeather = function(data) {
+  console.log(data.name);
+
+}
 // logic.display current year
 var copyrightYear = function() {
   var year = new Date().getFullYear();
+
   var copyrightEl = document.getElementById("copyright");
   copyrightEl.textContent = `\u00A9 ${year} `;
-  console.log(copyrightEl);
 };
 
 copyrightYear();
+getWeather("London");
